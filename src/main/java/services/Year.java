b5cd2f5a64be3bb6c71fb064ad487ebbd5e10174
Jegors Cemisovs <jegors.cemisovs@sap.com>
2021-06-28 21:43:51 +0300

    Add class Year

---
+++ src/main/java/services/Year.java
@@ -0,0 +1,14 @@
+package services;
+
+import java.time.LocalDate;
+
+public class Year extends AbstractReport {
+    public Year(final LocalDate birthday) {
+        super(birthday);
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
