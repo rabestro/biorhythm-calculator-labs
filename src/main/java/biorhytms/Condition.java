1cf687927acc0d0aa07dd82c93678ec7ce7c2be4
Jegors Cemisovs <jegors.cemisovs@sap.com>
2021-07-01 23:45:04 +0300

    Completed AnnualReport

--- src/main/java/biorhytms/Condition.java
+++ src/main/java/biorhytms/Condition.java
@@ -1,19 +1,19 @@
 package biorhytms;
 
 public enum Condition {
-    Well('*'),
-    Usually('+'),
-    Danger('0'),
-    Loose('-'),
-    Tired('.');
+    Well("*"),
+    Usually("+"),
+    Danger("0"),
+    Loose("-"),
+    Tired(".");
 
-    private final char symbol;
+    private final String symbol;
 
-    Condition(final char symbol) {
+    Condition(final String symbol) {
         this.symbol = symbol;
     }
 
-    public char getSymbol() {
+    public String getSymbol() {
         return symbol;
     }
 }
--- src/main/java/services/AnnualReport.java
+++ src/main/java/services/AnnualReport.java
@@ -4,9 +4,12 @@ import biorhytms.Biorhythm;
 
 import java.time.DateTimeException;
 import java.time.LocalDate;
+import java.time.Month;
 import java.time.Year;
+import java.time.format.TextStyle;
 import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
+import java.util.Locale;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -25,6 +28,18 @@ public class AnnualReport extends AbstractReport {
     @Override
     public void run() {
         System.out.println();
+        System.out.println("      Birthday: " + birthday + "           Annual report for " + year + " year");
+        System.out.println();
+        Function<Month, String> shortName = month -> month.getDisplayName(TextStyle.SHORT, Locale.ENGLISH);
+        final var headerMonths = "      " + Arrays
+                .stream(Month.values())
+                .map(shortName)
+                .collect(Collectors.joining("  "));
+
+        final var headerPEI = "    " + "  PEI".repeat(Month.values().length);
+        System.out.println(headerMonths);
+        System.out.println(headerPEI);
+
         for (int day = 1; day < 32; day++) {
             System.out.printf("%3d ", day);
             for (int month = 1; month < 13; month++) {
@@ -42,12 +57,9 @@ public class AnnualReport extends AbstractReport {
             if (days < 0) {
                 return EMPTY;
             }
-            Function<Biorhythm, Character> condition = biorhythm -> biorhythm.getCondition(days).getSymbol();
+            Function<Biorhythm, String> condition = biorhythm -> biorhythm.getCondition(days).getSymbol();
 
-            return Biorhythm.primary()
-                    .map(condition)
-                    .map(String::valueOf)
-                    .collect(Collectors.joining());
+            return Biorhythm.primary().map(condition).collect(Collectors.joining());
 
         } catch (DateTimeException e) {
             return EMPTY;
