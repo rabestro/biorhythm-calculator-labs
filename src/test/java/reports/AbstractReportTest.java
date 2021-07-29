98407d9cdd42f4afed65e5d8dcddd6dd8408adf1
Jegors Cemisovs <jegors.cemisovs@sap.com>
2021-07-29 08:37:06 +0300

    Add method for ordinal days. Test added.

---
+++ src/test/java/reports/AbstractReportTest.java
@@ -0,0 +1,19 @@
+package reports;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Tag("date")
+@DisplayName("Test ordinal days")
+class AbstractReportTest {
+
+    @ParameterizedTest(name = "{0} = {1}")
+    @CsvFileSource(resources = "/ordinal-days.csv", numLinesToSkip = 1)
+    void dayOrdinal(final int day, final String expected) {
+        assertEquals(expected, AbstractReport.dayOrdinal(day));
+    }
+}
