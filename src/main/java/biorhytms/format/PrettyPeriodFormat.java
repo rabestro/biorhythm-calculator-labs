1f898e7aba61c8d733212415d973fcb84bc88d41
Jegors Cemisovs <jegors.cemisovs@sap.com>
2021-08-04 09:11:28 +0300

    Add empty PrettyPeriodFormat

---
+++ src/main/java/biorhytms/format/PrettyPeriodFormat.java
@@ -0,0 +1,17 @@
+package biorhytms.format;
+
+import java.text.FieldPosition;
+import java.text.Format;
+import java.text.ParsePosition;
+
+public class PrettyPeriodFormat extends Format {
+    @Override
+    public StringBuffer format(final Object obj, final StringBuffer toAppendTo, final FieldPosition pos) {
+        return null;
+    }
+
+    @Override
+    public Object parseObject(final String source, final ParsePosition pos) {
+        return null;
+    }
+}
