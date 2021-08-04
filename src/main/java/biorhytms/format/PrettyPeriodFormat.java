ad7402edfaa3be236865da7ab3325cd762ac012e
Jegors Cemisovs <jegors.cemisovs@sap.com>
2021-08-04 09:34:34 +0300

    Updated implementation for PrettyPeriodFormat

--- src/main/java/biorhytms/format/PrettyPeriodFormat.java
+++ src/main/java/biorhytms/format/PrettyPeriodFormat.java
@@ -9,18 +9,22 @@ import java.text.ParsePosition;
 import java.time.Period;
 
 public class PrettyPeriodFormat extends Format {
+    private static final MessageFormat YEARS =
+            new MessageFormat("{0, choice, 0#|1#one year|2#two years|2<{0} years}");
+    private static final MessageFormat MONTHS =
+            new MessageFormat("{0, choice, 0#|1#one month|2#two months|2<{0} months}");
+    private static final MessageFormat DAYS =
+            new MessageFormat("{0, choice, 0#|1#one day|2#two days|2<{0} days}");
+
     @Override
     public StringBuffer format(final Object obj, final StringBuffer toAppendTo, final FieldPosition pos) {
         if (!(obj instanceof Period)) {
             throw new IllegalArgumentException("argument should be Period");
         }
         val period = (Period) obj;
-        val years = MessageFormat.format(
-                "{0, choice, 0#|1#one year|2#two years|2<{0} years}", period.getYears());
-        val months = MessageFormat.format(
-                "{0, choice, 0#|1#one month|2#two months|2<{0} months}", period.getMonths());
-        val days = MessageFormat.format(
-                "{0, choice, 0#|1#one day|2#two days|2<{0} days}", period.getDays());
+        val years = YEARS.format(new Object[]{period.getYears()});
+        val months = MONTHS.format(new Object[]{period.getYears()});
+        val days = DAYS.format(new Object[]{period.getYears()});
 
         if ((years + months + days).isEmpty()) {
             return toAppendTo.append("just born");
@@ -28,10 +32,14 @@ public class PrettyPeriodFormat extends Format {
 
         val one = years.isEmpty() || (months.isEmpty() && days.isEmpty())
                 ? "" : months.isEmpty() || days.isEmpty() ? " and " : ", ";
-
         val two = days.isEmpty() || months.isEmpty() ? "" : " and ";
 
-        return toAppendTo.append(years + one + months + two + days);
+        return toAppendTo
+                .append(years)
+                .append(one)
+                .append(months)
+                .append(two)
+                .append(days);
     }
 
     @Override
