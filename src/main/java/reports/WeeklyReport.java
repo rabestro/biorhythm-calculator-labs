d03694962114c302f2dc7c98a740c9c544be43f9
Jegors Cemisovs <jegors.cemisovs@sap.com>
2021-08-05 09:45:00 +0300

    Deleted outdated code from WeeklyReport

--- src/main/java/reports/WeeklyReport.java
+++ src/main/java/reports/WeeklyReport.java
@@ -6,17 +6,10 @@ import reports.format.WeeklyFormat;
 import java.text.Format;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoField;
 import java.util.stream.Stream;
 
 public class WeeklyReport extends AbstractReport {
-    private static final DateTimeFormatter FIRST_LINE = DateTimeFormatter.ofPattern("EEEE");
-    private static final DateTimeFormatter SECOND_LINE = DateTimeFormatter.ofPattern("MMMM ");
-    private static final DateTimeFormatter THIRD_LINE = DateTimeFormatter.ofPattern("yyyy");
-    private static final String LINE_SEPARATOR =
-            "+-----------------+------------------------------------------------------------";
-    private static final String TEMPLATE = LINE_SEPARATOR + "%n" + "| %-15s | %s%n".repeat(3);
     private static final Format DAILY_FORMAT = new WeeklyFormat("%-12s %4d %%");
 
     public WeeklyReport() {
@@ -31,34 +24,16 @@ public class WeeklyReport extends AbstractReport {
         new WeeklyReport().run();
     }
 
-    void printDay(final LocalDate localDate) {
-        System.out.format(TEMPLATE,
-                localDate.format(FIRST_LINE), Biorhythm.Physical.new Value(birthday(), localDate),
-                localDate.format(SECOND_LINE) + dayOrdinal(localDate.getDayOfMonth()),
-                Biorhythm.Emotional.new Value(birthday(), localDate),
-                localDate.format(THIRD_LINE), Biorhythm.Intellectual.new Value(birthday(), localDate)
-        );
-    }
-
     @Override
     public void run() {
-        println("report.weekly.top");
-        weekDays().forEach(day -> println("report.weekly.day",
-                LONG_DATE.format(day),
+        int weekOfYear = date().get(ChronoField.ALIGNED_WEEK_OF_YEAR);
+//        int weekOfYear = date.get(WeekFields.of(locale).weekOfYear());
+        printf("format.weekly.top", date().getYear(), weekOfYear);
+        weekDays().forEach(day -> printf("format.weekly.day", day,
                 DAILY_FORMAT.format(Biorhythm.Physical.new Value(birthday(), day)),
                 DAILY_FORMAT.format(Biorhythm.Emotional.new Value(birthday(), day)),
                 DAILY_FORMAT.format(Biorhythm.Intellectual.new Value(birthday(), day)))
         );
-    }
-
-    public void old() {
-        int weekOfYear = date().get(ChronoField.ALIGNED_WEEK_OF_YEAR);
-        System.out.println();
-        System.out.println("|  Weekly Report  |");
-//        int weekOfYear = date.get(WeekFields.of(locale).weekOfYear());
-        weekDays().forEach(this::printDay);
-
-        System.out.println(LINE_SEPARATOR);
     }
 
     private Stream<LocalDate> weekDays() {
