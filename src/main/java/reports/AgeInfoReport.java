9d95e9c7382dd695221a64b2317e5a932b4b89d0
Jegors Cemisovs <jegors.cemisovs@sap.com>
2021-08-05 10:35:32 +0300

    Refactoring for AgeInfoReport

--- src/main/java/reports/AgeInfoReport.java
+++ src/main/java/reports/AgeInfoReport.java
@@ -4,12 +4,9 @@ import biorhytms.ZodiacSign;
 import lombok.val;
 import reports.format.PrettyPeriodFormat;
 
-import java.text.Format;
 import java.time.Period;
 
 public class AgeInfoReport extends AbstractReport {
-    private final Format AGE_FORMAT = PrettyPeriodFormat.getInstance();
-
     public AgeInfoReport(ReportData reportData) {
         super(reportData);
     }
@@ -17,11 +14,9 @@ public class AgeInfoReport extends AbstractReport {
     @Override
     public void run() {
         val zodiacSign = ZodiacSign.of(birthday());
-        printf("format.ageInfo", birthday(), zodiacSign,
-                date(),
-                reportData.getDays(),
-                AGE_FORMAT.format(Period.between(birthday(), date()))
-        );
+        val age = PrettyPeriodFormat.getInstance().format(Period.between(birthday(), date()));
+
+        printf("format.ageInfo", birthday(), zodiacSign, date(), reportData.getDays(), age);
 
         if (zodiacSign.getLuckyDay().equals(date().getDayOfWeek())) {
             printf("format.luckyDay", zodiacSign.getLuckyDay(), zodiacSign.name());
