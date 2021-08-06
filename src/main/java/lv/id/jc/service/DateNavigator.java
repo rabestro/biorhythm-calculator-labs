aee197b731e3b875b62f92792a2745c6df296f64
Jegors Cemisovs <jegors.cemisovs@outlook.lv>
2021-08-06 23:19:19 +0300

    Add move operations for quarters

--- src/main/java/lv/id/jc/service/DateNavigator.java
+++ src/main/java/lv/id/jc/service/DateNavigator.java
@@ -5,6 +5,7 @@ import lv.id.jc.report.Context;
 import lv.id.jc.ui.LocalTextInterface;
 
 import java.time.LocalDate;
+import java.time.Period;
 import java.util.Map;
 import java.util.function.BiFunction;
 import java.util.regex.Pattern;
@@ -12,12 +13,14 @@ import java.util.regex.Pattern;
 import static java.lang.System.Logger.Level.TRACE;
 
 public class DateNavigator extends LocalTextInterface implements Runnable {
-    private static final Pattern PLUS_MINUS = Pattern.compile("([-+])(\\d+)([dwmy])");
-    private static final Map<String, BiFunction<LocalDate, Long, LocalDate>> operatorMap = Map.of(
+    private static final Pattern PLUS_MINUS = Pattern.compile("([-+])(\\d+)([dwmyq])");
+    private static final Map<String, BiFunction<LocalDate, Long, LocalDate>> moveOperator = Map.of(
             "+d", LocalDate::plusDays, "-d", LocalDate::minusDays,
-            "+w", LocalDate::plusWeeks, "-w", LocalDate::minusDays,
+            "+w", LocalDate::plusWeeks, "-w", LocalDate::minusWeeks,
             "+m", LocalDate::plusMonths, "-m", LocalDate::minusMonths,
-            "+y", LocalDate::plusYears, "-y", LocalDate::minusYears
+            "+y", LocalDate::plusYears, "-y", LocalDate::minusYears,
+            "+q", (d, n) -> d.plus(Period.of(0, n.intValue() * 3, 0)),
+            "-q", (d, n) -> d.minus(Period.of(0, n.intValue() * 3, 0))
     );
 
     private final Context context;
@@ -28,6 +31,7 @@ public class DateNavigator extends LocalTextInterface implements Runnable {
 
     @Override
     public void run() {
+        printf("welcome", context.birthday(), context.date());
         while (true) {
             printf("prompt", context.date());
             val command = scanner.nextLine().toLowerCase();
@@ -40,7 +44,7 @@ public class DateNavigator extends LocalTextInterface implements Runnable {
                 val number = Long.parseLong(plusMinus.group(2));
                 val unit = plusMinus.group(3);
                 LOGGER.log(TRACE, "sign = {0}, number = {1}, unit = {2}", sign, number, unit);
-                context.setDate(operatorMap.get(sign + unit).apply(context.date(), number));
+                context.setDate(moveOperator.get(sign + unit).apply(context.date(), number));
             }
         }
     }
