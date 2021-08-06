9bcbfbe2a2a66dca946026a24f5c952733cb80d4
Jegors Cemisovs <jegors.cemisovs@sap.com>
2021-08-06 05:07:07 +0300

    updated summary-report.properties

--- src/main/java/report/SummaryReport.java
+++ src/main/java/report/SummaryReport.java
@@ -2,13 +2,16 @@ package report;
 
 import biorhytms.Biorhythm;
 import report.format.BiorhythmTemplateFormat;
-import report.format.SummaryFormat;
+import report.format.DaysFormat;
+import report.format.OrdinalDateFormat;
 
 import java.text.Format;
 import java.util.stream.Stream;
 
 public class SummaryReport extends AbstractReport {
-    private static final Format SUMMARY_FORMAT = new SummaryFormat();
+    private static final Format ORDINAL_DATE_FORMAT = new OrdinalDateFormat();
+    private static final Format DAYS_FORMAT = new DaysFormat();
+    private final Format shortInfo = new BiorhythmTemplateFormat(getString("short.biorhythm.format"));
 
     public SummaryReport(final ReportData reportData) {
         super(reportData);
@@ -17,18 +20,21 @@ public class SummaryReport extends AbstractReport {
     @Override
     public void run() {
         printf("summary.header.format");
-        var formatter = new BiorhythmTemplateFormat(getString("short.biorhythm.format"));
-        biorhythms().map(formatter::format).forEach(this::println);
-
-        println();
-
-        biorhythms()
-                .map(SUMMARY_FORMAT::format)
-                .forEach(System.out::println);
+        biorhythms().map(shortInfo::format).forEach(this::println);
+        biorhythms().forEach(this::printInfo);
     }
 
     private Stream<Biorhythm.Value> biorhythms() {
         return Biorhythm.primary().map(biorhythm -> biorhythm.new Value(birthday(), date()));
     }
 
+    private void printInfo(Biorhythm.Value value) {
+        printf("summary.biorhythm.name.format", value.getBiorhythm());
+        printf(getString(value.getStage().name()),
+                value.getBiorhythm().name().toLowerCase(),
+                value.getBiorhythm().getAttributes(),
+                ORDINAL_DATE_FORMAT.format(value.cycleLastDay()),
+                DAYS_FORMAT.format(value.changesInDays())
+        );
+    }
 }
