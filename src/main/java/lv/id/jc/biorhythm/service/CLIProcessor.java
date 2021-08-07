03596f2996c6af49d049bdfddbac6b1f8743355d
Jegors Cemisovs <jegors.cemisovs@outlook.lv>
2021-08-07 20:22:28 +0300

    Implemented class CLIProcessor

--- src/main/java/lv/id/jc/biorhythm/service/CLIProcessor.java
+++ src/main/java/lv/id/jc/biorhythm/service/CLIProcessor.java
@@ -1,4 +1,24 @@
 package lv.id.jc.biorhythm.service;
 
-public class CLIProcessor {
+import lv.id.jc.biorhythm.Component;
+import lv.id.jc.biorhythm.Context;
+
+import java.time.LocalDate;
+
+public class CLIProcessor extends Component {
+    private final String[] args;
+
+    public CLIProcessor(final Context context, String... args) {
+        super(context);
+        this.args = args;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equalsIgnoreCase("--birthday")) {
+                context.setBirthday(LocalDate.parse(args[++i]));
+            }
+        }
+    }
 }
