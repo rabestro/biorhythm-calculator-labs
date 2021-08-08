b42e1cb31aabb084b0c41257e42a8d3b87010bd7
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2021-08-08 13:44:50 +0300

    Delete FormatTest.java

--- src/test/java/lv/id/jc/biorhythm/format/DaysFormatTest.java
+++ src/test/java/lv/id/jc/biorhythm/format/DaysFormatTest.java
@@ -5,21 +5,23 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
+import java.text.Format;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Tag("Formatter")
-class DaysFormatTest extends FormatTest {
-    static final String CSV = "/days-format.csv";
+class DaysFormatTest {
+    private Format underTest;
 
     @BeforeEach
     void setUp() {
-        format = new DaysFormat();
+        underTest = new DaysFormat();
     }
 
     @ParameterizedTest(name = "{0} = {1}")
-    @CsvFileSource(resources = CSV, numLinesToSkip = 1)
+    @CsvFileSource(resources = "/days-format.csv", numLinesToSkip = 1)
     void dayOrdinal(final int day, final String expected) {
-        assertEquals(expected, format.format(day));
+        assertEquals(expected, underTest.format(day));
     }
-
+    
 }
