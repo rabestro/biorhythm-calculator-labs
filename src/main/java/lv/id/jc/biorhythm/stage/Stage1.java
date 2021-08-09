7070ff3581bbdc202f659a5bc4ab7d0021583b07
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2021-08-09 17:52:52 +0300

    Add Stage1

---
+++ src/main/java/lv/id/jc/biorhythm/stage/Stage1.java
@@ -0,0 +1,18 @@
+package lv.id.jc.biorhythm.stage;
+
+import lv.id.jc.biorhythm.Context;
+import lv.id.jc.biorhythm.report.AgeInfoReport;
+import lv.id.jc.biorhythm.report.ZodiacSignReport;
+import lv.id.jc.biorhythm.service.BirthdayRequester;
+
+import java.time.LocalDate;
+
+public class Stage1 {
+    public static void main(String[] args) {
+        final var context = new Context(LocalDate.MIN);
+
+        new BirthdayRequester(context).run();
+        new AgeInfoReport(context).run();
+        new ZodiacSignReport(context).run();
+    }
+}
