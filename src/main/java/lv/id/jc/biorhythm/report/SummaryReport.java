bb6296868e4155d67c6ccbfa6ad61b677e1da2d3
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2021-08-10 17:48:43 +0300

    Deleted unused OrdinalDateFormat.java

--- src/main/java/lv/id/jc/biorhythm/report/SummaryReport.java
+++ src/main/java/lv/id/jc/biorhythm/report/SummaryReport.java
@@ -1,10 +1,7 @@
 package lv.id.jc.biorhythm.report;
 
 import lv.id.jc.biorhythm.Context;
-import lv.id.jc.biorhythm.format.BiorhythmFormat;
-import lv.id.jc.biorhythm.format.DaysFormat;
-import lv.id.jc.biorhythm.format.MultilineTextFormat;
-import lv.id.jc.biorhythm.format.OrdinalDateFormat;
+import lv.id.jc.biorhythm.format.*;
 import lv.id.jc.biorhythm.model.Biorhythm;
 import lv.id.jc.biorhythm.ui.Component;
 
@@ -12,7 +9,6 @@ import java.text.Format;
 import java.util.stream.Stream;
 
 public class SummaryReport extends Component {
-    private static final Format ORDINAL_DATE_FORMAT = new OrdinalDateFormat();
     private static final Format DAYS_FORMAT = new DaysFormat();
     private final Format shortInfoFormat = new BiorhythmFormat(getString("short.biorhythm.format"));
     private final Format multilineFormat = new MultilineTextFormat();
@@ -33,11 +29,12 @@ public class SummaryReport extends Component {
     }
 
     private void printInfo(Biorhythm.Value value) {
+        final var formatter = new MonthOrdinalDay();
         printf("summary.biorhythm.name.format", value.getBiorhythm());
         print(multilineFormat.format(format(getString(value.getStage().name()),
                 value.getBiorhythm().name().toLowerCase(),
                 value.getBiorhythm().getAttributes(),
-                ORDINAL_DATE_FORMAT.format(value.changeDate()),
+                formatter.format(value.changeDate()),
                 DAYS_FORMAT.format(value.changeInDays()))));
     }
 }
