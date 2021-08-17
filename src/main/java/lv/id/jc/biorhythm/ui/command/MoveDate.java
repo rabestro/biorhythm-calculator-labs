fea6ac78877cd9f7ebf58d336805f610c05ac07d
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2021-08-17 18:03:36 +0300

    Add examples for Monthly Report

--- src/main/java/lv/id/jc/biorhythm/ui/command/MoveDate.java
+++ src/main/java/lv/id/jc/biorhythm/ui/command/MoveDate.java
@@ -30,21 +30,6 @@ public class MoveDate extends AbstractCommand {
     }
 
     @Override
-    public boolean test(String command) {
-        final var plusMinus = COMMAND_PATTERN.matcher(command);
-        final var isValidCommand = plusMinus.matches();
-
-        if (isValidCommand) {
-            final var sign = plusMinus.group(1);
-            final var unit = plusMinus.group(3);
-            final var number = Long.parseLong(plusMinus.group(2));
-            LOGGER.log(TRACE, "sign = {0}, number = {1}, unit = {2}", sign, number, unit);
-            runnable = () -> setDate(MOVE_OPERATORS.get(sign + unit).apply(date(), number));
-        }
-        return isValidCommand;
-    }
-
-    @Override
     public Boolean apply(String request) {
         final var matcher = COMMAND_PATTERN.matcher(command);
         if (!matcher.matches()) {
