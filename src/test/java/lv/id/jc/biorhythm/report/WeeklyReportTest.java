1c32b0673805c7c2d4458f73406881bd2784ad18
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2021-08-18 16:59:11 +0300

    Add test for WeeklyReportTest

--- src/test/java/lv/id/jc/biorhythm/report/WeeklyReportTest.java
+++ src/test/java/lv/id/jc/biorhythm/report/WeeklyReportTest.java
@@ -1,17 +1,23 @@
 package lv.id.jc.biorhythm.report;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class WeeklyReportTest {
+class WeeklyReportTest extends AbstractReportTest {
 
-    @BeforeEach
-    void setUp() {
+    WeeklyReportTest() {
+        super(WeeklyReport::new);
     }
 
     @Test
     void run() {
+        underTest.run();
+
+        final var actual = out.toString();
+
+        System.err.println(actual);
+
+        assertTrue(actual.contains("Weekly"));
     }
 }
