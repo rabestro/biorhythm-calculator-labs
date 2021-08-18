ba1334397fbe2386bc8ad611d6b1b3cc36056374
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2021-08-18 20:15:13 +0300

    Fixed pretty-period-format.csv

--- src/main/java/lv/id/jc/biorhythm/format/PrettyPeriodFormat.java
+++ src/main/java/lv/id/jc/biorhythm/format/PrettyPeriodFormat.java
@@ -10,11 +10,11 @@ import java.time.Period;
 
 public class PrettyPeriodFormat extends Format {
     private static final MessageFormat YEARS =
-            new MessageFormat("{0, choice, 0#|1#one year|2#two years|2<{0} years}");
+            new MessageFormat("{0, choice, 0#|1#one year|1<{0} years}");
     private static final MessageFormat MONTHS =
-            new MessageFormat("{0, choice, 0#|1#one month|2#two months|2<{0} months}");
+            new MessageFormat("{0, choice, 0#|1#one month|1<{0} months}");
     private static final MessageFormat DAYS =
-            new MessageFormat("{0, choice, 0#|1#one day|2#two days|2<{0} days}");
+            new MessageFormat("{0, choice, 0#|1#one day|1<{0} days}");
 
     public static PrettyPeriodFormat getInstance() {
         return new PrettyPeriodFormat();
