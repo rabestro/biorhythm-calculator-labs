cd4d2648f0d47fb789fb7981e347425e7a8d6fc5
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2021-08-19 02:51:00 +0300

    Add tests for Ordinal Date Formatter

--- src/test/java/lv/id/jc/biorhythm/format/DateFormatterTest.java
+++ src/test/java/lv/id/jc/biorhythm/format/DateFormatterTest.java
@@ -1,30 +1,30 @@
 package lv.id.jc.biorhythm.format;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 import java.time.LocalDate;
 import java.time.MonthDay;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @Tag("Formatter")
 @DisplayName("Given DateFormatter")
 class DateFormatterTest {
+    static final String INCORRECT_DATE = "32-MAY";
 
     private DateTimeFormatter underTest;
 
     @Nested
     @DisplayName("Given ROMAN_FULL")
-    class FullRomanDate {
+    class RomanFullDate {
         @BeforeEach
         void setUp() {
-            underTest = DateFormatter.ROMAN_FULL;
+            underTest = DateFormatter.ROMAN_FULL_DATE;
         }
 
         @ParameterizedTest(name = "when format {0} then {1}")
@@ -39,13 +39,20 @@ class DateFormatterTest {
             final var actual = underTest.parse(roman, LocalDate::from);
             assertEquals(expected, actual);
         }
+
+        @Test
+        @DisplayName("when parse incorrect date then throws DateTimeParseException")
+        void incorrectDate() {
+            assertThrows(DateTimeParseException.class, () -> underTest.parse(INCORRECT_DATE));
+        }
     }
+
     @Nested
     @DisplayName("Given ROMAN_SHORT")
     class RomanShortDate {
         @BeforeEach
         void setUp() {
-            underTest = DateFormatter.ROMAN_SHORT;
+            underTest = DateFormatter.ROMAN_SHORT_DATE;
         }
 
         @ParameterizedTest(name = "when format {0} then {1}")
@@ -60,5 +67,63 @@ class DateFormatterTest {
             final var actual = underTest.parse(roman, MonthDay::from);
             assertEquals(expected, actual);
         }
+
+        @Test
+        @DisplayName("when parse incorrect date then throws DateTimeParseException")
+        void incorrectDate() {
+            assertThrows(DateTimeParseException.class, () -> underTest.parse(INCORRECT_DATE));
+        }
+
+    }
+
+    @Nested
+    @DisplayName("Given ORDINAL_SHORT")
+    class OrdinalShortDate {
+        @BeforeEach
+        void setUp() {
+            underTest = DateFormatter.ORDINAL_SHORT_DATE;
+        }
+
+        @ParameterizedTest(name = "when format {0} then {1}")
+        @CsvFileSource(resources = "/format/ordinal-short.csv", numLinesToSkip = 1)
+        void formatShortOrdinal(final MonthDay date, final String expected) {
+            assertEquals(expected, underTest.format(date));
+        }
+
+        @ParameterizedTest(name = "when parse {1} then {0}")
+        @CsvFileSource(resources = "/format/ordinal-short.csv", numLinesToSkip = 1)
+        void parseShortOrdinal(final MonthDay expected, final String roman) {
+            final var actual = underTest.parse(roman, MonthDay::from);
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        @DisplayName("when parse incorrect date then throws DateTimeParseException")
+        void incorrectDate() {
+            assertThrows(DateTimeParseException.class, () -> underTest.parse(INCORRECT_DATE));
+        }
+
+    }
+
+    @Nested
+    @DisplayName("Given ORDINAL_FULL")
+    class OrdinalFullDate {
+        @BeforeEach
+        void setUp() {
+            underTest = DateFormatter.ORDINAL_FULL_DATE;
+        }
+
+        @ParameterizedTest(name = "when format {0} then {1}")
+        @CsvFileSource(resources = "/format/ordinal-full.csv", numLinesToSkip = 1)
+        void formatFullOrdinal(final LocalDate date, final String expected) {
+            assertEquals(expected, underTest.format(date));
+        }
+
+        @Test
+        @DisplayName("when parse incorrect date then throws DateTimeParseException")
+        void incorrectDate() {
+            assertThrows(DateTimeParseException.class, () -> underTest.parse(INCORRECT_DATE));
+        }
+
     }
 }
