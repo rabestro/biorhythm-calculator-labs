0750a3e05e5764a3ffe225ded1c720b684d78698
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2021-08-20 22:28:12 +0300

    #110 Add InfoTest

---
+++ src/test/java/lv/id/jc/biorhythm/command/InfoTest.java
@@ -0,0 +1,27 @@
+package lv.id.jc.biorhythm.command;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static java.time.LocalDate.EPOCH;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class InfoTest extends AbstractDateCommand {
+    @BeforeEach
+    void setUp() {
+        super.setUp();
+        underTest = new Info(context);
+    }
+
+    @ParameterizedTest(name = "when request {0} then is recognized: {1}")
+    @CsvFileSource(resources = "/command/info-request.csv", numLinesToSkip = 1)
+    void recognized(final String request) {
+        final var result = underTest.test(request);
+
+        assertTrue(result);
+        assertEquals(BIRTHDAY, context.birthday(), "birthday shall be unchanged");
+        assertEquals(EPOCH, context.date(), "date shall be unchanged");
+    }
+}
